3d0a4fbbb9a40e97126c84016fc6ed698924ada3
magedss <imagedsayedmob07@gmail.com>
2025-12-24 12:44:02 +0200

    shm

--- Serialaization.c
+++ Serialaization.c
@@ -6,5 +6,23 @@
 static void buf_write_u32(char *buffer, uint64_t *buf_size, uint64_t buf_capacity,uint32_t x){
     assert(*buf_size + sizeof(x) <= buf_capacity);
     assert(((*buf_size) + (size_t)buffer) % sizeof(x) == 0);
+   *(uint32_t *)(buffer+ (*buf_size))=x;
+    *buf_size+=sizeof(x);
 
 } 
+static void buf_write_u16(char *buffer, uint64_t *buf_size, uint64_t buf_capacity,uint16_t x){
+    assert(*buf_size + sizeof(x) <= buf_capacity);
+    assert(((*buf_size) + (size_t)buffer) % sizeof(x) == 0);
+   *(uint32_t *)(buffer+ (*buf_size))=x;
+    *buf_size+=sizeof(x);
+
+} 
+static uint32_t buf_read_u32(char **buffer, uint64_t *buf_size){
+    assert(((size_t)*buffer%sizeof(uint32_t))==0);
+    assert(*buf_size>=sizeof(uint32_t));
+    uint32_t result= *(uint32_t*)*buffer;
+    *buffer+=sizeof(result);
+    *buf_size-=sizeof(result);
+    return result;
+
+}
--- opcode.c
+++ opcode.c
@@ -1,4 +1,11 @@
-static const uint32_t wayland_display_object_id = 1;
+
+
+#include <stdint.h>
+#include <stddef.h>
+#include <assert.h>
+#ifndef WAYLAND_STUFF_H
+#define WAYLAND_STUFF_H
+static const uint32_t  wayland_display_object_id= 1;
 
  static const uint16_t wayland_wl_registry_event_global = 0;
 
@@ -43,3 +50,52 @@ static const uint32_t wayland_display_object_id = 1;
  static const uint32_t wayland_header_size = 8;
 
  static const uint32_t color_channels = 4;
+
+ enum state_state_t {
+
+   STATE_NONE,
+
+   STATE_SURFACE_ACKED_CONFIGURE,
+
+   STATE_SURFACE_ATTACHED,
+
+ };
+
+ struct state_t {
+
+   uint32_t wl_registry;
+
+   uint32_t wl_shm;
+
+   uint32_t wl_shm_pool;
+
+   uint32_t wl_buffer;
+
+   uint32_t xdg_wm_base;
+
+   uint32_t xdg_surface;
+
+   uint32_t wl_compositor;
+
+   uint32_t wl_surface;
+
+   uint32_t xdg_toplevel;
+
+   uint32_t stride;
+
+   uint32_t w;
+
+   uint32_t h;
+
+   uint32_t shm_pool_size;
+
+   int shm_fd;
+
+   uint8_t *shm_pool_data;
+
+ 
+
+   enum state_state_t state;
+
+ };
+#endif
